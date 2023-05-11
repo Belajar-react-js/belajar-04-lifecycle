02cc2a904d4a758c9d65bdec578036536997597f
kharisma tara mukti <kharismataram@gmail.com>
2023-05-11 10:50:17 +0700

    menambhakna axion jangan lupa install

--- src/Components/ListDigimon.jsx
+++ src/Components/ListDigimon.jsx
@@ -1,30 +1,41 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 
 function ListDigimon() {
-  const [digimon, setDigimon] = useState([]);
+  const [digimons, setDigiomons] = useState([]);
   console.log(1);
 
   useEffect(() => {
-    //https://digimon-api.vercel.app/api/digimon
-    //https://jsonsplaceholder.typecode.com/users
-
-    fetch("https://digimon-api.vercel.app/api/digimon")
-      .then((response) => response.json())
-      .then((result) => {
-        console.log(result);
-        setDigimon(result);
-      });
+    // fetch("https://digimon-api.vercel.app/api/digimon")
+    //   .then((response) => response.json())
+    //   .then((result) => {
+    //     setDigiomons(result);
+    //   });
+    axios("https://digimon-api.vercel.app/api/digimon")
+      .then(({ data }) => setDigiomons(data))
+      .catch((err) => console.log(err));
 
     console.log(3);
   }, []);
-  console.log(digimon);
+
+  console.log(digimons);
+
   return (
-    <>
-      <div>
-        <h1>Digimons</h1>
-        {console.log(2)}
-      </div>
-    </>
+    <div>
+      <h1>Digimons</h1>
+      {console.log(2)}
+      {digimons.length === 0 ? (
+        <span>Loading...</span>
+      ) : (
+        digimons.map((item, index) => (
+          <div key={index}>
+            <img src={item.img} alt="" width={200} />
+            <h3>{item.name}</h3>
+            <h4>{item.level}</h4>
+          </div>
+        ))
+      )}
+    </div>
   );
 }
 
